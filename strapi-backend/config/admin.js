caf5ba21dff5bbc502da9ecc16cf9640a221e321
DebnarPatrik <patrik.debnar03@e-uvt.ro>
2026-07-02 13:01:46 +0000

    Fix: add default values for admin config salts

--- strapi-backend/config/admin.js
+++ strapi-backend/config/admin.js
@@ -1,13 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'myAdminJwtSecret123456'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'myApiTokenSalt123456'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'myTransferTokenSalt123'),
     },
+  },
+  flags: {
+    nps: env.bool('FLAG_NPS', true),
+    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
